153dc9df4df55cd8bbab852ada3c2e971a3fbed9
contrazap <contrazap@gmail.com>
2023-10-04 11:07:00 +0530

    Removed unused import

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,6 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
-import {
-  Row,
-  Col,
-  Image,
-  ListGroup,
-  Button,
-  Card,
-  ListGroupItem,
-} from "react-bootstrap";
+import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
 import Rating from "../components/Rating";
 import products from "../products";
 
