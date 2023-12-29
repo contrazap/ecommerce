5a55791692feb29ee6f6ad274b0b4d5e7d4c01ec
contrazap <contrazap@gmail.com>
2023-12-29 14:33:16 +0530

    use products from db

--- backend/base/views.py
+++ backend/base/views.py
@@ -3,7 +3,9 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Product
 from .products import products
+from .serializers import ProductSerializer
 
 # Create your views here.
 
@@ -25,15 +27,15 @@ def getRoutes(request):
 
 @api_view(["GET"])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+
+    return Response(serializer.data)
 
 
 @api_view(["GET"])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i["_id"] == pk:
-            product = i
-            break
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
 
-    return Response(product)
+    return Response(serializer.data)
